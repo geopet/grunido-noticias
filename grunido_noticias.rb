116b9d4fc818f0310bdd936355f0ef8031d502a9
Geoff Petrie <g.petrie@gmail.com>
2013-11-25 12:04:30 -0700

    Update growl display
    I've changed the growl display to show all current headlines. Getting
    the first top headline wasn't showing the news I had hoped. This gives
    me all the current headlines, which provides a little more information.
    
    This may prove to be too much. Maybe allow for a parameter to be passed
    for number? I definitely also want to pass what sort of news we want to
    receive as well.

--- grunido_noticias.rb
+++ grunido_noticias.rb
@@ -5,12 +5,13 @@ require 'growl'
 require 'feedzirra'
 
 feed = Feedzirra::Feed.fetch_and_parse("http://news.google.com/news?pz=1&cf=all&ned=us&hl=en&output=rss")
-entry = feed.entries.first
 
-notify = Growl.new
-notify.title = 'Noticias!'
-notify.message = entry.title
-notify.url = entry.url
-notify.sticky!
+feed.entries.each do |entry|
+  notify = Growl.new
+  notify.title = 'Noticias!'
+  notify.message = entry.title
+  notify.url = entry.url
+  notify.sticky!
 
-notify.run
+  notify.run
+end
